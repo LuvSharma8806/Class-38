98e983aa13e6112bc96ce357b08b798ed12b3b8c
unknown <urva09sharma@gmail.com>
2020-10-20 17:52:21 +0530

    Adding images

--- sketch.js
+++ sketch.js
@@ -9,6 +9,17 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var track,bg,car1Img,car2Img,car3Img,car4Img;
+
+function preload(){
+  track=loadImage("../images/track.jpg");
+  bg=loadImage("../images/track.png");
+  car1Img=loadImage("../images/car1.png");
+  car2Img=loadImage("../images/car2.png");
+  car3Img=loadImage("../images/car3.png");
+  car4Img=loadImage("../images/car4.png");
+  
+}
 
 
 function setup(){
@@ -27,5 +38,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if (gameState===2){
+    game.end();
   }
 }
